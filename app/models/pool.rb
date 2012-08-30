b2a1578b9d7b0aa7213765d7da0e8fae7e5d9db0
Tom Fenton <tom@ncs.co.nz>
2012-08-31 06:09:55 +1200

    Add validations, ignore vim swap files.

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,3 +1,10 @@
 class Pool < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :city, :name
+
+  belongs_to :swim
+
+  validates :name, :presence => true
+  validates :city, :presence => true
+
+  default_scope order("name asc")
 end
--- app/models/swim.rb
+++ app/models/swim.rb
@@ -1,3 +1,9 @@
 class Swim < ActiveRecord::Base
   attr_accessible :concession_card, :kilometers, :pool_id, :program, :purchase, :swam_on
+
+  has_one :pool
+
+  validates :swam_on, :pool_id, :kilometers, :presence => true
+  validates :kilometers, :numericality => true
+  validates :concession_card, :numericality => { :only_integer => true }
 end
