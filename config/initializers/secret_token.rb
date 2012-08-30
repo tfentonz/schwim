cd9388fd37c5471302b753bc6616a19c523f0364
Tom Fenton <tom@ncs.co.nz>
2012-08-30 20:36:51 +1200

    Create a new Rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schwim::Application.config.secret_token = 'f4e8e41e7769eb49ed69b7d18ed0561b1307d83852fa53b17da30fa8701d8f11551cf9a0ef16eb9243d8a54dca3183fade6e1c5888621edfb16bcd534ec0d5d2'
