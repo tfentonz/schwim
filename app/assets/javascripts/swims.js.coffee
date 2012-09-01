05efc8ef0238dc03967881a33c7be65167846e0d
Tom Fenton <tom@ncs.co.nz>
2012-09-01 18:32:07 +1200

    Update swim views. Use jQuery UI datepicker.

--- app/assets/javascripts/swims.js.coffee
+++ app/assets/javascripts/swims.js.coffee
@@ -1,3 +1,9 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+jQuery ->
+  $('#swim_swam_on').datepicker
+    maxDate: 0
+
+  $('.ui-datepicker-trigger').addClass('btn')
--- app/models/swim.rb
+++ app/models/swim.rb
@@ -4,9 +4,15 @@ class Swim < ActiveRecord::Base
 
   belongs_to :pool
 
+  after_initialize :init
+
   validates :swam_on, :presence => true
   validates :pool_id, :kilometers, :presence => true
   validates :kilometers, :numericality => true
   validates :concession_card, :numericality => { :only_integer => true },
             :allow_nil => true
+
+  def init
+    self.swam_on ||= Date.today
+  end
 end
