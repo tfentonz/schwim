6e2fbe7a547189cae982c2d29050254f618fcc7e
Tom Fenton <tom@ncs.co.nz>
2012-09-03 23:49:44 +1200

    Swim unit test.

--- test/unit/swim_test.rb
+++ test/unit/swim_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class SwimTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should default to todays date" do
+    swim = Swim.new
+    assert_equal swim.swam_on, Date.today
+  end
 end
